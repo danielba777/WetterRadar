eb8e4c11305cb6c5a3dbcf393fea9d13da250fb9
danielba777 <danielbauer230501@gmail.com>
2024-04-20 21:28:06 +0200

    Responsive Behaviour changed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,7 +6,7 @@ export default function Body(props) {
   const {geoData,weatherData,days,weekDays,selDate,setSelDate,extractData,getWeatherCode,nextDay} = props
 
   return (
-    <section className='flex flex-col items-center mt-8 w-full p-0'>    
+    <section className='flex flex-col items-center justify-center mt-8 w-full p-0'>    
         <div className='flex flex-col items-start w-full md:w-1/2'>
             <h1 className='text-2xl ml-4 md:ml-0 md:text-2xl lg:text-4xl'>Das Wetter heute in {geoData?.city}</h1>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='w-full flex flex-col items-center'>
-        <div className='w-1/2 flex flex-col justify-start items-center gap-8 py-8'>
-            <hr className='w-full'></hr>
+    <div className='w-full flex flex-col items-center p-8'>
+        <p className='text-xl'>Folgen Sie WetterRadar auf</p>
+        <div className='flex gap-4 pb-8 pt-4'>
+          <button><i class="fa-brands fa-facebook fa-xl"></i></button>
+          <button><i class="fa-brands fa-instagram fa-xl"></i></button>
+          <button><i class="fa-brands fa-square-x-twitter fa-xl"></i></button>
+          <button><i class="fa-brands fa-linkedin fa-xl"></i></button>
+        </div>
+        <hr className='w-full'></hr>
+        <div className='flex flex-col items-center gap-4 py-4'>
+          <p>Datenschutz & Cookies</p>
+          <p>Kontakt & Support</p>
+          <p>Impressum</p>
+          <p>Compliance</p>
+        </div>
+        <hr className='w-full'></hr>
+        <div className='w-full flex flex-col justify-start items-center gap-8 py-4'>
             <div className='flex items-center gap-1'>
                 <i className="fa-regular fa-copyright h-4"></i> 
                 <p className='h-4'>2024 WetterRadar - alle Rechte vorbehalten</p>

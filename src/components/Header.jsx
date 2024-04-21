fbf88b3e82156d8036c077cfaa4a6d9ceb456906
danielba777 <danielbauer230501@gmail.com>
2024-04-21 09:36:38 +0200

    Updated Logo path in the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header(props) {
     <>
     <div className='w-full flex flex-col items-center'>
       <div className='w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 flex flex-col sm:flex-row justify-start items-center gap-4 p-4'>
-        <img className='h-8 w-40' src='./public/WetterRadar_Logo.png'/>
+        <img className='h-8 w-40' src='/WetterRadar_Logo.png'/>
         <div className='flex justify-between items-center w-full sm:w-72 bg-white h-11 rounded-3xl text-xl text-slate-500 '>
           <input type='text' id='userInput' className='rounded-3xl pl-4 focus:outline-none focus:ring-0 w-full' placeholder='Deine Stadt...' onKeyDown={(e) =>{
             if(e.key === "Enter"){
